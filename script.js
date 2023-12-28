b47eb4caf6992f80e71df69e1c287d2cf168a2e3
Dhruv Varshney <144780592+dhruv996@users.noreply.github.com>
2023-12-28 14:26:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function firstChar(text) {
-  // your code here
+document.addEventListener('DOMContentLoaded', function () {
+  // Example usage
+  const result1 = firstChar(' Rosa Parks ');
+  document.getElementById('result').innerText = `Result for ' Rosa Parks ': '${result1}'`;
+
+  const result2 = firstChar(' Hello World ');
+  document.getElementById('result').innerText += `\nResult for ' Hello World ': '${result2}'`;
+
+  const result3 = firstChar(' ');
+  document.getElementById('result').innerText += `\nResult for ' ': '${result3}'`;
+
+  const result4 = firstChar('');
+  document.getElementById('result').innerText += `\nResult for '': '${result4}'`;
+});
+
+function firstChar(str) {
+  for (const char of str) {
+    if (char !== ' ') {
+      return char;
+    }
+  }
+  return '';
 }
 
-// Do not change the code below
-
-const text = prompt("Enter text:");
-alert(firstChar(text));
